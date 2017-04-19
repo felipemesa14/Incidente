e86dbe013c779960bd3e151d6bfe241b844e33f6
pc--25 <pc--25@pc25-H81M-H>
2017-04-19 14:59:54 -0500

    informacion

--- src/AppBundle/Controller/web/WebController.php
+++ src/AppBundle/Controller/web/WebController.php
@@ -14,13 +14,29 @@ class WebController extends Controller {
 
         return $this->render('AppBundle:Web:index.html.twig');
     }
-    
+
     /**
      * @Route("/contacto", name="web_contacto")
      */
     public function contactoAction() {
 
         return $this->render('AppBundle:Web:contacto.html.twig');
+    }
+
+    /**
+     * @Route("/corporativo", name="web_corporativo")
+     */
+    public function corporativoAction() {
+
+        return $this->render('AppBundle:Web:corporativo.html.twig');
+    }
+
+    /**
+     * @Route("/cliente", name="web_cliente")
+     */
+    public function clienteAction() {
+
+        return $this->render('AppBundle:Web:cliente.html.twig');
     }
 
 }
